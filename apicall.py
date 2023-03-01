2e34f03d5edf7acb2b2ed8046e717ae2c1285d87
itsayushs <ayushstarksharma@gmail.com>
2023-03-02 00:21:34 +0530

    added code to dev branch

--- apicall.py
+++ apicall.py
@@ -2,6 +2,7 @@ import requests
 import json
 from random import randrange
 
+
 def getData():
     random_number = str(randrange(10))
     x = requests.get('https://reqres.in/api/users/'+random_number)
